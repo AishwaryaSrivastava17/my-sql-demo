c4ead10f45960cc241b5ca38bc5038173bb5eefb
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-04-08 13:53:22 +0530

    update api

--- src/main/java/com/example/mysqldemo/controller/UserController.java
+++ src/main/java/com/example/mysqldemo/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.example.mysqldemo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +23,30 @@ public class UserController {
 	private IuserService userService;
 	@PostMapping("user")
 	int createUser(@RequestBody User user){
+		
 		return userService.save(user);
+	}
+	
+	@GetMapping("user")
+	List<User> getusers() {
+		return userService.getUsers();
+	}
+
+	@GetMapping("user/{id}")
+	Optional<User> getUser(@PathVariable("id") Integer userId) {
+		return userService.getUser(userId);
+	}
+	private void validateModel(Errors bindingResult) {
+		if (bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Something went wrong. Please retry");
+		}
+	}
+
+	@PutMapping("user/{id}")
+	void updateUser(@RequestBody @Value(value = "") User user, BindingResult bindingResult,
+			@PathVariable("id") Integer userId) {
+		validateModel(bindingResult);
+		user.setId(userId);
+		userService.updateUser(user);
 	}
 }
